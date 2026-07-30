401954d3a453042a31f28c2d7595603014ee95f2
JPriester <85929216+JPriester@users.noreply.github.com>
2026-07-30 23:28:14 +0000

    Deploying to gh-pages from @ JPriester/JPriester.github.io@5218a20f06f8ae0fcf79e97417343aa7855e5302 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "Academic and professional curriculum vitae.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
